7e2c7d111649cb03780e5343ab27656adec91aad
Shyam Sreenivasan <shyam.vegeta@gmail.com>
2024-06-12 03:23:08 +0530

    loadComponent

--- apps/conduit/src/app/app.component.ts
+++ apps/conduit/src/app/app.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
   standalone: true,
   imports: [RouterModule],
-  selector: 'app-root',
+  // eslint-disable-next-line @angular-eslint/component-selector
+  selector: 'cdt-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
   title = 'conduit';
--- apps/conduit/src/app/app.routes.ts
+++ apps/conduit/src/app/app.routes.ts
@@ -2,14 +2,14 @@ import { Route } from '@angular/router';
 
 export const appRoutes: Route[] = [
   {
+    path: 'register',
+    loadComponent: () =>
+      import('@realword/auth/feature-auth').then((c) => c.RegisterComponent),
+  },
+  {
     path: 'login',
     loadComponent: () =>
       import('@realword/auth/feature-auth').then((c) => c.LoginComponent),
-  },
-  {
-    path: 'register',
-    loadChildren: () =>
-      import('@realword/auth/feature-auth').then((c) => c.RegisterComponent),
   },
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: '**', redirectTo: 'login', pathMatch: 'full' },
