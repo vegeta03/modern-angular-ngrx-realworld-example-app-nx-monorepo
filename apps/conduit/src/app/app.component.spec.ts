78e95a2472b32af3b2fa44eaaa58a24533684787
Shyam Sreenivasan <shyam.vegeta@gmail.com>
2024-06-12 01:20:54 +0530

    initial setup

--- apps/conduit/src/app/app.component.spec.ts
+++ apps/conduit/src/app/app.component.spec.ts
@@ -1,12 +1,13 @@
 import { TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
+
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
-import { RouterTestingModule } from '@angular/router/testing';
+import { appRoutes } from './app.routes';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent, NxWelcomeComponent, RouterTestingModule],
+      imports: [AppComponent, provideRouter(appRoutes)],
     }).compileComponents();
   });
 
