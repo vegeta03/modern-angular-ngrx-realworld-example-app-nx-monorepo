b129c52b32870d50eff9fcf8f7cabc62a78c210a
Shyam Sreenivasan <shyam.vegeta@gmail.com>
2024-06-12 03:02:12 +0530

    withComponentInputBinding(), withViewTransitions()

--- apps/conduit/src/app/app.config.ts
+++ apps/conduit/src/app/app.config.ts
@@ -1,10 +1,19 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {
+  provideRouter,
+  withComponentInputBinding,
+  withViewTransitions,
+} from '@angular/router';
+
 import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(appRoutes),
+    provideRouter(
+      appRoutes,
+      withComponentInputBinding(),
+      withViewTransitions()
+    ),
   ],
 };
